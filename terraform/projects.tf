a6e2639c727c3cee35f1f2cbac6a20235c627f8a
VuVanNghia <vuvannghia.work@gmail.com>
2026-02-14 10:23:51 +0700

    Update projects variable to include countdown commands services

--- terraform/projects.tf
+++ terraform/projects.tf
@@ -3,7 +3,11 @@ variable "projects" {
   description = "Danh sách tên các project cần tạo"
   default = [
     "kong",
+
     "dev-api-gateway-http-log",
     "prod-api-gateway-http-log",
+
+    "dev-countdown-commands-service",
+    "prod-countdown-commands-service",
   ]
 }
